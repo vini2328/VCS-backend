7178862aad3d2a8b89257b94f79216b32578650c
vini2328 <vinitayarda7@gmail.com>
2023-05-29 17:00:12 +0530

    added changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 const router= express.Router();
 import UserController from "../controllers/userController.js";
+import checkUserAuth from "../middlewares/auth-middleware.js";
 
 // route level middleware - To protect  Route
+router.use('/changepassword',checkUserAuth)
+router.use('/loggeduser',checkUserAuth)
 
 
 // public routes
@@ -11,6 +14,8 @@ router.post('/login',UserController.userLogin)
 
 
 // protected routes
+router.post('/changepassword',UserController.changeUserPassword)
+router.get('/loggeduser',UserController.loggedUser)
 
 
 
